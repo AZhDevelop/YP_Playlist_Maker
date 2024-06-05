438e4640dc3c4ff41c9c6ef2af0b29361b8268ae
AZhDevelop <azh.develop@mail.ru>
2024-06-05 13:17:44 +0300

    Added several activities and a simple navigation

--- app/src/main/java/com/example/yp_playlist_maker/MainActivity.kt
+++ app/src/main/java/com/example/yp_playlist_maker/MainActivity.kt
@@ -1,20 +1,54 @@
 package com.example.yp_playlist_maker
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.view.View.OnClickListener
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        /** Реализация нажатия на кнопку "Поиск" с помощью анонимного класса */
+
+        val searchButton = findViewById<Button>(R.id.search)
+        val displaySearchIntent = Intent(this, SearchActivity::class.java)
+        val searchButtonClickListener: View.OnClickListener = object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                startActivity(displaySearchIntent)
+            }
+        }
+
+        searchButton.setOnClickListener(searchButtonClickListener)
+
+        /** Реализация нажатия на кнопку "Медиатека" с помощью лямбда-выражения */
+
+        val mediaButton = findViewById<Button>(R.id.media)
+        mediaButton.setOnClickListener {
+            val displayMediaIntent = Intent(this, MediaActivity::class.java)
+            startActivity(displayMediaIntent)
+        }
+
+        /** Реализация нажатия на кнопку "Настройки" с помощью MainActivity */
+
+        val settingsButton = findViewById<Button>(R.id.settings)
+        settingsButton.setOnClickListener(this@MainActivity)
+
+    }
+
+    override fun onClick(v: View?) {
+        when (v?.id) {
+            R.id.settings -> {
+                val displayIntent = Intent(this, SettingsActivity::class.java)
+                startActivity(displayIntent)
+            }
         }
     }
+
 }
