59c4a1f861d3080120d3508874446fec9e3f360b
AZhDevelop <azh.develop@mail.ru>
2024-06-22 20:23:52 +0300

    Added on SettingsActivity license agreement intent

--- app/src/main/java/com/example/yp_playlist_maker/MainActivity.kt
+++ app/src/main/java/com/example/yp_playlist_maker/MainActivity.kt
@@ -2,13 +2,8 @@ package com.example.yp_playlist_maker
 
 import android.content.Intent
 import android.os.Bundle
-import android.view.View
-import android.view.View.OnClickListener
 import android.widget.Button
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
--- app/src/main/java/com/example/yp_playlist_maker/SettingsActivity.kt
+++ app/src/main/java/com/example/yp_playlist_maker/SettingsActivity.kt
@@ -1,6 +1,9 @@
 package com.example.yp_playlist_maker
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.widget.FrameLayout
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -10,5 +13,17 @@ class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
+
+        val shareButton = findViewById<FrameLayout>(R.id.fl_share)
+        val contactSupportButton = findViewById<FrameLayout>(R.id.fl_contact_support)
+        val licenseAgreementButton = findViewById<FrameLayout>(R.id.fl_license_agreement)
+
+        licenseAgreementButton.setOnClickListener {
+            val url = "https://yandex.ru/legal/practicum_offer"
+            val licenseAgreementIntent = Intent(Intent.ACTION_VIEW)
+            licenseAgreementIntent.data = Uri.parse(url)
+            startActivity(licenseAgreementIntent)
+        }
+
     }
 }
