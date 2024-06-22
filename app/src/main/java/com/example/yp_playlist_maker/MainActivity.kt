82d254b19c59608c473f4726015bfc65d86f1930
AZhDevelop <azh.develop@mail.ru>
2024-06-22 19:44:04 +0300

    MainActivity Button's Intents have been changed

--- app/src/main/java/com/example/yp_playlist_maker/MainActivity.kt
+++ app/src/main/java/com/example/yp_playlist_maker/MainActivity.kt
@@ -10,45 +10,31 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity(), OnClickListener {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        /** Реализация нажатия на кнопку "Поиск" с помощью анонимного класса */
-
         val searchButton = findViewById<Button>(R.id.btn_search)
-        val displaySearchIntent = Intent(this, SearchActivity::class.java)
-        val searchButtonClickListener: View.OnClickListener = object : View.OnClickListener {
-            override fun onClick(v: View?) {
-                startActivity(displaySearchIntent)
-            }
+        val mediaButton = findViewById<Button>(R.id.btn_media)
+        val settingsButton = findViewById<Button>(R.id.btn_settings)
+
+        searchButton.setOnClickListener {
+            val displaySearchIntent = Intent(this, SearchActivity::class.java)
+            startActivity(displaySearchIntent)
         }
 
-        searchButton.setOnClickListener(searchButtonClickListener)
-
-        /** Реализация нажатия на кнопку "Медиатека" с помощью лямбда-выражения */
-
-        val mediaButton = findViewById<Button>(R.id.btn_media)
         mediaButton.setOnClickListener {
             val displayMediaIntent = Intent(this, MediaActivity::class.java)
             startActivity(displayMediaIntent)
         }
 
-        /** Реализация нажатия на кнопку "Настройки" с помощью MainActivity */
-
-        val settingsButton = findViewById<Button>(R.id.btn_settings)
-        settingsButton.setOnClickListener(this)
-
-    }
-
-    override fun onClick(v: View?) {
-        when (v?.id) {
-            R.id.btn_settings -> {
-                val displayIntent = Intent(this, SettingsActivity::class.java)
-                startActivity(displayIntent)
-            }
+        settingsButton.setOnClickListener {
+            val displaySettingsIntent = Intent(this, SettingsActivity::class.java)
+            startActivity(displaySettingsIntent)
         }
+
+
     }
 
 }
