c8282b83bddce4c31f4de3153b99af4fef3e44e7
AZhDevelop <azh.develop@mail.ru>
2024-06-23 19:40:50 +0300

    Sprint nine second homework commit and added blue color to Switch

--- app/src/main/java/com/example/yp_playlist_maker/SettingsActivity.kt
+++ app/src/main/java/com/example/yp_playlist_maker/SettingsActivity.kt
@@ -18,10 +18,30 @@ class SettingsActivity : AppCompatActivity() {
         val contactSupportButton = findViewById<FrameLayout>(R.id.fl_contact_support)
         val licenseAgreementButton = findViewById<FrameLayout>(R.id.fl_license_agreement)
 
+        shareButton.setOnClickListener() {
+            val shareIntent = Intent().apply {
+                action = Intent.ACTION_SEND
+                type = "text/plain"
+                putExtra(Intent.EXTRA_TEXT, getString(R.string.share_link))
+            }
+            startActivity(Intent.createChooser(shareIntent, getString(R.string.share_with)))
+        }
+
+        contactSupportButton.setOnClickListener {
+            val shareIntent = Intent().apply {
+                action = Intent.ACTION_SENDTO
+                data = Uri.parse(getString(R.string.contact_support_mailto))
+                putExtra(Intent.EXTRA_SUBJECT, getString(R.string.contact_support_subject))
+                putExtra(Intent.EXTRA_TEXT, getString(R.string.contact_support_message))
+            }
+            startActivity(shareIntent)
+        }
+
         licenseAgreementButton.setOnClickListener {
-            val url = "https://yandex.ru/legal/practicum_offer"
-            val licenseAgreementIntent = Intent(Intent.ACTION_VIEW)
-            licenseAgreementIntent.data = Uri.parse(url)
+            val licenseAgreementIntent = Intent().apply {
+                action = Intent.ACTION_VIEW
+                data = Uri.parse(getString(R.string.license_agreement_link))
+            }
             startActivity(licenseAgreementIntent)
         }
 
