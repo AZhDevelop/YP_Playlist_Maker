768cd8d1e07135be3884013e72d30989791c425d
AZhDevelop <azh.develop@mail.ru>
2024-06-30 12:10:04 +0300

    Sprint nine third homework commit

--- app/src/main/java/com/example/yp_playlist_maker/SearchActivity.kt
+++ app/src/main/java/com/example/yp_playlist_maker/SearchActivity.kt
@@ -1,14 +1,49 @@
 package com.example.yp_playlist_maker
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import android.widget.EditText
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class SearchActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
+
+        val clearText = findViewById<ImageView>(R.id.iw_clear)
+        val editText = findViewById<EditText>(R.id.et_search)
+
+        clearText.visibility = View.INVISIBLE
+
+        clearText.setOnClickListener {
+            editText.setText("")
+        }
+
+        val simpleTextWatcher = object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                //
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                clearText.visibility = clearButtonVisibility(s)
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                //
+            }
+        }
+        editText.addTextChangedListener(simpleTextWatcher)
     }
+
+    private fun clearButtonVisibility(s: CharSequence?): Int {
+        return if (s.isNullOrEmpty()) {
+            View.INVISIBLE
+        } else {
+            View.VISIBLE
+        }
+    }
+
 }
