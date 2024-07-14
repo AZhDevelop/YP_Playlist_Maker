75b5b3baf1b6c50e7934c39cc76fde449ea34cb2
AZhDevelop <azh.develop@mail.ru>
2024-07-14 20:22:58 +0300

    Added conveter dpToPx for Glide

--- app/src/main/java/com/example/yp_playlist_maker/TrackViewHolder.kt
+++ app/src/main/java/com/example/yp_playlist_maker/TrackViewHolder.kt
@@ -1,18 +1,25 @@
 package com.example.yp_playlist_maker
 
+import android.content.Context
+import android.content.res.Resources
+import android.util.DisplayMetrics
+import android.util.TypedValue
 import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+import kotlin.math.roundToInt
 
-class TrackViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+class TrackViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
     private val trackName: TextView = itemView.findViewById(R.id.track_name)
     private val artistName: TextView = itemView.findViewById(R.id.track_artist)
     private val trackTime: TextView = itemView.findViewById(R.id.track_length)
     private val artworkUrl100: ImageView = itemView.findViewById(R.id.track_image)
+
+    private val pixelSizeToCorners : Int = 2
 
     fun bind(item: Track) {
         trackName.text = item.trackName
@@ -22,9 +29,19 @@ class TrackViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
         Glide.with(itemView)
             .load(item.artworkUrl100)
             .centerCrop()
-            .transform(RoundedCorners(2))
+            .transform(RoundedCorners(pixelSizeToCorners.dpToPx()))
             .placeholder(R.drawable.image_placeholder)
             .into(artworkUrl100)
     }
 
+    private fun Int.dpToPx(): Int {
+        val displayMetrics = Resources.getSystem().displayMetrics
+        return (this * (displayMetrics.densityDpi / DisplayMetrics.DENSITY_DEFAULT.toFloat())).roundToInt()
+    }
+
 }
+
+
+
+
+
