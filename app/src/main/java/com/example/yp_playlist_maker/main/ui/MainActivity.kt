18c36858a3127fa8789ea6b9c868caa2f91476d6
AZhDevelop <azh.develop@mail.ru>
2024-11-22 16:08:47 +0300

    First steps to single activity app

--- app/src/main/java/com/example/yp_playlist_maker/main/ui/MainActivity.kt
+++ app/src/main/java/com/example/yp_playlist_maker/main/ui/MainActivity.kt
@@ -1,36 +1,16 @@
 package com.example.yp_playlist_maker.main.ui
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import com.example.yp_playlist_maker.media.ui.MediaActivity
-import com.example.yp_playlist_maker.R
-import com.example.yp_playlist_maker.search.ui.SearchActivity
-import com.example.yp_playlist_maker.settings.ui.SettingsActivity
+import com.example.yp_playlist_maker.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        val searchButton = findViewById<Button>(R.id.btn_search)
-        val mediaButton = findViewById<Button>(R.id.btn_media)
-        val settingsButton = findViewById<Button>(R.id.btn_settings)
-
-        searchButton.setOnClickListener {
-            val displaySearchIntent = Intent(this, SearchActivity::class.java)
-            startActivity(displaySearchIntent)
-        }
-
-        mediaButton.setOnClickListener {
-            val displayMediaIntent = Intent(this, MediaActivity::class.java)
-            startActivity(displayMediaIntent)
-        }
-
-        settingsButton.setOnClickListener {
-            val displaySettingsIntent = Intent(this, SettingsActivity::class.java)
-            startActivity(displaySettingsIntent)
-        }
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
