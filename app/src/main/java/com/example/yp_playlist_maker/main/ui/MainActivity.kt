11008b768362b964def09f2763b4c9507f7d360c
AZhDevelop <azh.develop@mail.ru>
2024-11-23 22:40:10 +0300

    Added BottomNavigationView

--- app/src/main/java/com/example/yp_playlist_maker/main/ui/MainActivity.kt
+++ app/src/main/java/com/example/yp_playlist_maker/main/ui/MainActivity.kt
@@ -3,6 +3,9 @@ package com.example.yp_playlist_maker.main.ui
 import android.content.pm.ActivityInfo
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.example.yp_playlist_maker.R
 import com.example.yp_playlist_maker.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -14,6 +17,12 @@ class MainActivity : AppCompatActivity() {
         requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.main) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        binding.bottomNavigationView.setupWithNavController(navController)
+
 
     }
 }
