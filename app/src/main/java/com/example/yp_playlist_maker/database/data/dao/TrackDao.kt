c5097fc0f12552b98018a6e411387783e97fcd38
AZhDevelop <azh.develop@mail.ru>
2024-12-26 14:54:04 +0300

    Added new DAO interface TrackDao

---
+++ app/src/main/java/com/example/yp_playlist_maker/database/data/dao/TrackDao.kt
@@ -0,0 +1,22 @@
+package com.example.yp_playlist_maker.database.data.dao
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.example.yp_playlist_maker.database.data.entity.TrackEntity
+
+@Dao
+interface TrackDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertTrack(track: TrackEntity)
+
+    @Delete
+    suspend fun deleteTrack(track: TrackEntity)
+
+    @Query("SELECT * FROM track_table")
+    suspend fun getTrackList(): List<TrackEntity>
+
+}
