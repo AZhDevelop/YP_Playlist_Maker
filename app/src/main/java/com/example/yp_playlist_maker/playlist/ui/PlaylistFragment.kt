b37b05a288e781bc1610b71346212a3c3d2f0598
AZhDevelop <azh.develop@mail.ru>
2025-01-09 18:39:04 +0300

    Some code refactor and added button isEnabled check when all edittexts are empty/non-empty

--- app/src/main/java/com/example/yp_playlist_maker/playlist/ui/PlaylistFragment.kt
+++ app/src/main/java/com/example/yp_playlist_maker/playlist/ui/PlaylistFragment.kt
@@ -7,6 +7,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.EditText
+import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.example.yp_playlist_maker.R
@@ -14,7 +16,7 @@ import com.example.yp_playlist_maker.app.gone
 import com.example.yp_playlist_maker.app.visible
 import com.example.yp_playlist_maker.databinding.FragmentPlaylistBinding
 
-class PlaylistFragment: Fragment() {
+class PlaylistFragment : Fragment() {
 
     private var _binding: FragmentPlaylistBinding? = null
     private val binding get() = _binding!!
@@ -39,11 +41,12 @@ class PlaylistFragment: Fragment() {
             findNavController().navigateUp()
         }
 
+        setFragmentElements()
         setEditTextWatchers()
 
     }
 
-    private fun setTextWatcher(editText: EditText) : TextWatcher {
+    private fun setTextWatcher(editText: EditText, textView: TextView): TextWatcher {
         val simpleTextWatcher = object : TextWatcher {
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
                 //
@@ -52,16 +55,12 @@ class PlaylistFragment: Fragment() {
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
                 if (editText.text.isEmpty()) {
                     editText.setBackgroundResource(R.drawable.playlist_empty_text_drawable)
-                    binding.apply {
-                        tvPlaylistName.gone()
-                        tvPlaylistDescription.gone()
-                    }
+                    textView.gone()
+                    checkButtonIsAvailable()
                 } else {
                     editText.setBackgroundResource(R.drawable.playlist_text_drawable)
-                    binding.apply {
-                        tvPlaylistName.visible()
-                        tvPlaylistDescription.visible()
-                    }
+                    textView.visible()
+                    checkButtonIsAvailable()
                 }
             }
 
@@ -74,12 +73,35 @@ class PlaylistFragment: Fragment() {
     }
 
     private fun setEditTextWatchers() {
-        val playlistName = binding.etPlaylistName
-        val playlistDescription = binding.etPlaylistDescription
-        _playlistNameTextWatcher = setTextWatcher(playlistName)
-        _playlistDescriptionTextWatcher = setTextWatcher(playlistDescription)
-        playlistName.addTextChangedListener(playlistNameTextWatcher)
-        playlistDescription.addTextChangedListener(playlistDescriptionTextWatcher)
+        val etPlaylistName = binding.etPlaylistName
+        val etPlaylistDescription = binding.etPlaylistDescription
+        val tvPlaylistName = binding.tvPlaylistName
+        val tvPlaylistDescription = binding.tvPlaylistDescription
+        _playlistNameTextWatcher = setTextWatcher(etPlaylistName, tvPlaylistName)
+        _playlistDescriptionTextWatcher =
+            setTextWatcher(etPlaylistDescription, tvPlaylistDescription)
+        etPlaylistName.addTextChangedListener(playlistNameTextWatcher)
+        etPlaylistDescription.addTextChangedListener(playlistDescriptionTextWatcher)
+    }
+
+    private fun setFragmentElements() {
+        binding.btnCreatePlaylist.isEnabled = false
+        binding.tvPlaylistName.gone()
+        binding.tvPlaylistDescription.gone()
+    }
+
+    private fun checkButtonIsAvailable() {
+        if (binding.etPlaylistName.text.isNotEmpty() && binding.etPlaylistDescription.text.isNotEmpty()) {
+            binding.btnCreatePlaylist.apply {
+                isEnabled = true
+                setBackgroundColor(ContextCompat.getColor(requireContext(), R.color.yp_blue))
+            }
+        } else {
+            binding.btnCreatePlaylist.apply {
+                isEnabled = false
+                setBackgroundColor(ContextCompat.getColor(requireContext(), R.color.yp_gray))
+            }
+        }
     }
 
 
