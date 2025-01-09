ae4d19977bd42b03c6cc8dfe7a2d6e544ed38300
AZhDevelop <azh.develop@mail.ru>
2025-01-09 14:22:01 +0300

    Added navigation bar visibility check to MainActivity

--- app/src/main/java/com/example/yp_playlist_maker/main/ui/MainActivity.kt
+++ app/src/main/java/com/example/yp_playlist_maker/main/ui/MainActivity.kt
@@ -2,10 +2,13 @@ package com.example.yp_playlist_maker.main.ui
 
 import android.content.pm.ActivityInfo
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.example.yp_playlist_maker.R
+import com.example.yp_playlist_maker.app.gone
+import com.example.yp_playlist_maker.app.visible
 import com.example.yp_playlist_maker.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -20,6 +23,16 @@ class MainActivity : AppCompatActivity() {
 
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.main) as NavHostFragment
         val navController = navHostFragment.navController
+
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            Log.d("log", "Current destination: ${destination.id}")
+            Log.d("log", "Playlist fragment id: ${R.id.fragment_playlist}")
+            if (destination.id == R.id.playlistFragment) {
+                binding.bottomNavigationView.gone()
+            } else {
+                binding.bottomNavigationView.visible()
+            }
+        }
 
         binding.bottomNavigationView.setupWithNavController(navController)
     }
